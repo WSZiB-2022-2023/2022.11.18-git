0ff207898506abd8d8342a806f6b03de69e239aa
Mateusz <mat.bereda@gmail.com>
2022-11-18 12:03:45 +0100

    Jakis drugi commit

--- src/main/java/pl/edu/wszib/git/Main.java
+++ src/main/java/pl/edu/wszib/git/Main.java
@@ -3,5 +3,7 @@ package pl.edu.wszib.git;
 public class Main {
     public static void main(String[] args) {
         System.out.println("git !!");
+
+        System.out.println("drugi commit !!!");
     }
 }
